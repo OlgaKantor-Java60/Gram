77b8bbe3e2fb3cb1462cd13b5f861d7d5d1397bd
OlgaKantor <kntrolga@gmail.com>
2025-03-21 22:44:05 +0200

    work in progress

--- src/script.js
+++ src/script.js
@@ -1,11 +1,26 @@
+
+const gallery = document.getElementById('gallery');
 const detailedImage = document.querySelector(".detailedContainer--image");
 const detailedTitle = document.querySelector(".detailedContainer--title");
 const galleryImages = document.querySelectorAll(".gallery--item_image");
-for (let i = 0; i < galleryImages.length; i++) {
-  galleryImages[i].addEventListener("click", function () {
-    setDetails(galleryImages[i]);
-  });
+
+drawImages(gallery);
+
+async function drawImages(element) {
+  const response = await fetch("https://api.thecatapi.com/v1/breeds");
+  const data = await response.json();
+  const images = getImages(data);
+  const types = getTypes(data);
+  const details = getDetailes(data);
+  const items = getItems(images, types, details);
+  element.innerHTML = items;
+  for (let i = 0; i < galleryImages.length; i++) {
+    galleryImages[i].addEventListener("click", function () {
+      setDetails(galleryImages[i]);
+    });
+  }
 }
+
 function setDetails(galleryImage) {
   let image = galleryImage.getAttribute("data-detailed-image");
   detailedImage.src = "";
@@ -15,6 +30,35 @@ function setDetails(galleryImage) {
     '<span class="for_ellipsis">...</span>';
   animate();
 }
+
+function getImages(data){
+  const images = data.map(record => `https://cdn2.thecatapi.com/images/${record.reference_image_id}.jpg`);
+  return images;
+}
+
+function getTypes(data){
+  const types = data.map(record => `${record.name}`);
+  return types;
+}
+
+function getDetailes(data){
+  const details = data.map(record => `${record.description}`);
+  return details;
+}
+
+function getItems(images, types, details){
+  const items = images.map(getItem, types, details);
+  return items.join('');
+}
+
+function getItem(image, type, detail){
+  const item = `<li class="gallery--item">
+  <img src =${image} alt="${type} cat" class="gallery--item_image data-detailed-image=${image} data-detailed-title= ${detail}>
+  <span class="gallery--item_title">${type} cat</span>
+  </li>`
+  return item;
+}
+
 function animate() {
   detailedImage.classList.remove("animation-up");
   detailedTitle.classList.remove("animation-down");
@@ -23,3 +67,4 @@ function animate() {
     detailedTitle.classList.add("animation-down");
   }, 0);
 }
+
