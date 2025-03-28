e795e0b81d6640c5c25f0c03e951924b2dae8912
OlgaKantor <kntrolga@gmail.com>
2025-03-28 14:40:20 +0300

    Movies 1994

--- src/script.js
+++ src/script.js
@@ -3,20 +3,25 @@ const gallery = document.getElementById("gallery");
 const detailedImage = document.querySelector(".detailedContainer--image");
 const detailedTitle = document.querySelector(".detailedContainer--title");
 let galleryImages;
+const urlImages = 'https://www.themoviedb.org/t/p/w1280/'
 
 drawGalleryItems();
 
 async function drawGalleryItems() {
-  const response = await fetch("https://api.thecatapi.com/v1/breeds");
+  const response = await fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&primary_release_year=1994&sort_by=popularity.desc', {
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNTkzZmQ0ZjAxYWQyMWU1NmUxMTZlZGM4MzI2NDM4NSIsIm5iZiI6MTc0MzA4MDk5OC41MTUsInN1YiI6IjY3ZTU0ZTI2Zjg0Njc5NGU5OTEwYWY4NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7A7Xpob6q6Lt7WsFIUwMAGUmTlnapruvzWDFQ9gvX_4'
+    }
+  })
   const data = await response.json();
-  const itemsData = getItemsData(data); //input data from API, output array of objects 
-   // {itemImage, detailedImage, title, detailedTitle}
+  const itemsData = getItemsData(data);
   const items = getItems(itemsData);
   gallery.innerHTML = items;
   galleryImages = document.querySelectorAll(".gallery--item_image");
   addListeners();
 }
-blabala
+
 function addListeners() {
   for (let i = 0; i < galleryImages.length; i++) {
     galleryImages[i].addEventListener("click", function () {
@@ -35,18 +40,17 @@ function setDetails(galleryImage) {
 }
 
 function getItemsData(data) {
-  const itemsData = data.map(record => ({
-    itemImage: getImage(record.reference_image_id), 
-    detailedImage: getImage(record.reference_image_id),
-    title: record.name,
-    detailedTitle: record.description
+  const itemsData = data.results.map(record => ({
+    itemImage: getImage(record.poster_path), 
+    detailedImage: getImage(record.backdrop_path),
+    title: record.title,
+    detailedTitle: record.overview
   }))
   return itemsData;
 }
 
 function getImage(id) {
-  return `https://cdn2.thecatapi.com/images/${id}.jpg`
-
+  return `${urlImages}${id}`
 }
 
 function getItems(itemsData) {
@@ -59,11 +63,11 @@ function getItem({itemImage, detailedImage, title, detailedTitle}) {
   <li class = "gallery--item">
   <img 
     src = "${itemImage}" 
-    alt = "${title} cat image" 
+    alt = "${title} image" 
     class = "gallery--item_image" 
     data-detailed-image = "${detailedImage}"
     data-detailed-title = "${detailedTitle}">
-  <span class = "gallery--item_title">${title} cat</span>
+  <span class = "gallery--item_title">${title}</span>
   </li>`;
   return item;
 }
